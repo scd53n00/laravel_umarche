fc9858cc5457854c526dcd40d75a7349a415d088
scd53n00 <scd53n00@gmail.com>
2022-12-30 06:32:56 +0900

    first

---
+++ docker/mysql/initdb.d/1_initalize.sql
@@ -0,0 +1,4 @@
+GRANT ALL privileges ON *.* to root@'%' identified by 'Root-2022' with grant option;
+GRANT ALL privileges ON *.* to laravel@'%' identified by 'Laravel-2022' with grant option;
+
+FLUSH PRIVILEGES;
